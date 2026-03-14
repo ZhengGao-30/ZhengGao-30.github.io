622572c1ab8784674b4fcc2d0bf3038ca9e0f61f
Amit Das <wdbamit@gmail.com>
2026-03-14 18:37:40 +0530

    Fix build errors for deployment

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -32,7 +32,7 @@ export default function setSplitText() {
     });
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      para.split?.words ?? [],
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -48,17 +48,20 @@ export default function setSplitText() {
       }
     );
   });
+
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
     title.anim = gsap.fromTo(
-      title.split.chars,
+      title.split?.chars ?? [],
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
